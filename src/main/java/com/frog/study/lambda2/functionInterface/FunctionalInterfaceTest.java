2650255ad9f0ca90002b1d0587f62a9f61f6ad28
shaohaichao <shaohaichao@shanshu.ai>
2021-03-10 18:42:13 +0800

    list set&add

--- src/main/java/com/frog/study/lambda2/functionInterface/FunctionalInterfaceTest.java
+++ src/main/java/com/frog/study/lambda2/functionInterface/FunctionalInterfaceTest.java
@@ -1,12 +1,12 @@
 package com.frog.study.lambda2.functionInterface;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.Objects;
+import java.util.function.*;
 
 /**
  * @author haichao.shao (shaohaichao@shanshu.ai)
@@ -44,7 +44,49 @@ public class FunctionalInterfaceTest {
         consumer.accept(value);
     }
 
+    /**
+     * 带索引遍历
+     *
+     * @param elements 集合
+     * @param action   消费
+     * @param <T>      泛型
+     */
+    private static <T> void forEach(Iterable<? extends T> elements, BiConsumer<Integer, ? super T> action) {
+        Objects.requireNonNull(elements);
+        Objects.requireNonNull(action);
+        //从0开始遍历的索引
+        int startIndex = 0;
+        int index = 0;
+        for (T element : elements) {
+            index++;
+            if (index <= startIndex) {
+                continue;
+            }
+            action.accept(index - 1, element);
+        }
+    }
+
     public static void main(String[] args) {
+        //5.测试带索引遍历
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> lista = Arrays.asList(1, 2, 3);
+        List<Integer> listb = Arrays.asList(4, 5, 6);
+        List<Integer> listc = Arrays.asList(7, 8, 9);
+        lists.add(lista);
+        lists.add(listb);
+        lists.add(listc);
+        List<Integer> result = new ArrayList<>();
+        lists.forEach(l -> forEach(l, (index, item) -> {
+            if (result.size() < l.size()) {
+                result.add(index, item + fetchById(result, index));
+            } else {
+                result.set(index, item + fetchById(result, index));
+            }
+        }));
+        System.out.println(result);
+    }
+
+    public static void main1(String[] args) {
         List<String> list = Arrays.asList("hello", "java8", "function", "predicate");
         FunctionalInterfaceTest test = new FunctionalInterfaceTest();
         //1.测试断言性接口
@@ -65,6 +107,24 @@ public class FunctionalInterfaceTest {
 
         //4.测试消费型接口
         test.modifyValue(3, s -> System.out.println(s * 3));
+
+        //5.测试带索引遍历
+        List<List<Integer>> lists = new ArrayList<>();
+        List<Integer> lista = Arrays.asList(1, 2, 3);
+        List<Integer> listb = Arrays.asList(4, 5, 6);
+        List<Integer> listc = Arrays.asList(7, 8, 9);
+        lists.add(lista);
+        lists.add(listb);
+        lists.add(listc);
+        List<Integer> result = new ArrayList<>();
+        lists.forEach(l -> forEach(l, (index, item) -> result.set(index, item + fetchById(result, index))));
+        System.out.println(result);
     }
 
+    static int fetchById(List<Integer> list, int i) {
+        if (CollectionUtils.isEmpty(list) || list.size() - 1 < i) {
+            return 0;
+        }
+        return list.get(i);
+    }
 }
