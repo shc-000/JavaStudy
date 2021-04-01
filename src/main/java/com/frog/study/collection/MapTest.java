38dfd46c552d94676ffa08ce21eda5d4ad897724
shaohaichao <shaohaichao@shanshu.ai>
2021-04-01 11:34:58 +0800

    map

---
+++ src/main/java/com/frog/study/collection/MapTest.java
@@ -0,0 +1,139 @@
+package com.frog.study.collection;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author haichao.shao (shaohaichao@shanshu.ai)
+ * @since 2021/3/30
+ */
+public class MapTest {
+
+    @Test
+    public void containsKey() {
+
+    }
+
+    @Test
+    public void containsValue() {
+
+    }
+
+    //compute() 方法对 hashMap 中指定 key 的值进行重新计算。指定的key在map中的值进行操作 不管存不存在，操作完成后保存到map中
+    //如果 key 对应的 value 不存在，则返回该 null，如果存在，则返回通过 remappingFunction 重新计算后的值。
+    @Test
+    public void compute() {
+        //统计一个字符串中各个单词出现的频率，然后从中找出频率最高的单词。
+        String str = "hello java, i am vary happy! nice to meet you";
+
+        // jdk1.8的写法
+        HashMap<Character, Integer> result2 = new HashMap<>(32);
+        for (int i = 0; i < str.length(); i++) {
+            char curChar = str.charAt(i);
+            result2.compute(curChar, (k, v) -> {
+                if (v == null) {
+                    v = 1;
+                } else {
+                    v += 1;
+                }
+                return v;
+            });
+        }
+    }
+
+    /*
+    //java8之前。从map中根据key获取value操作可能会有下面的操作
+    Object key = map.get("key");
+    if (key == null) {
+        key = new Object();
+        map.put("key", key);
+    }
+
+    // java8之后。上面的操作可以简化为一行，若key对应的value为空，会将第二个参数的返回值存入并返回
+    Object key2 = map.computeIfAbsent("key", k -> new Object());*/
+
+    //computeIfAbsent的方法有两个参数 第一个是所选map的key，第二个是需要做的操作。这个方法当key值不存在时才起作用。
+    //当key存在返回当前value值，不存在执行函数并保存到map中
+    @Test
+    public void computeIfAbsent() {
+        Map<String, List<Integer>> map = new HashMap<>();
+        //putIfAbsent在放入数据时，如果存在重复的key，那么putIfAbsent不会放入值。
+        //computeIfAbsent在放入数据时，若key对应的value为空，会将第二个参数的返回值存入并返回
+        map.computeIfAbsent("a", l -> new ArrayList<>()).add(6);
+        System.out.println(map);
+    }
+
+
+    //computeIfPresent 的方法,对 指定的 在map中已经存在的key的value进行操作。只对已经存在key的进行操作，其他不操作
+    @Test
+    public void computeIfPresent() {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        //只对map中存在的key对应的value进行操作
+        Integer integer = map.computeIfPresent("3", (k, v) -> v + 1);
+        Integer integer1 = map.computeIfPresent("4", (k, v) -> {
+            if (v == null) {
+                return 0;
+            }
+            return v + 1;
+        });
+        System.out.println(integer);
+        System.out.println(integer1);
+        System.out.println(map.toString());
+    }
+
+
+    /*merge() 可以这么理解：它将新的值赋值到 key （如果不存在）或更新给定的key 值对应的 value，
+    该方法接收三个参数，一个 key 值，一个 value，一个 remappingFunction ，
+    如果给定的key不存在，它就变成了 put(key, value) 。但是，如果 key 已经存在一些值，
+    我们  remappingFunction 可以选择合并的方式，然后将合并得到的 newValue 赋值给原先的 key。
+
+    这个使用场景相对来说还是比较多的，比如分组求和这类的操作，虽然 stream 中有相关 groupingBy() 方法，
+    但如果你想在循环中做一些其他操作的时候，merge() 还是一个挺不错的选择的。
+    */
+    @Test
+    public void merge() throws JsonProcessingException {
+        //假设我们有这么一段业务逻辑，我有一个学生成绩对象的列表，对象包含学生姓名、科目、科目分数三个属性，要求求得每个学生的总成绩。可以用merge
+        Map<String, Integer> studentScoreMap2 = new HashMap<>();
+        List<StudentScore> studentScoreList = buildTestList();
+        studentScoreList.forEach(studentScore -> studentScoreMap2.merge(
+                studentScore.getStuName(),
+                studentScore.getScore(),
+                Integer::sum));
+        ObjectMapper objectMapper = new ObjectMapper();
+        System.out.println(objectMapper.writeValueAsString(studentScoreMap2));
+    }
+
+    private List<StudentScore> buildTestList() {
+        List<StudentScore> studentScoreList = new ArrayList<>();
+        StudentScore studentScore1 = new StudentScore() {{
+            setStuName("张三");
+            setSubject("语文");
+            setScore(70);
+        }};
+        StudentScore studentScore2 = new StudentScore() {{
+            setStuName("张三");
+            setSubject("语文");
+            setScore(71);
+        }};
+        studentScoreList.add(studentScore1);
+        studentScoreList.add(studentScore2);
+        return studentScoreList;
+    }
+
+    @Data
+    public static class StudentScore {
+        private String stuName;
+        private Integer score;
+        private String subject;
+    }
+}
